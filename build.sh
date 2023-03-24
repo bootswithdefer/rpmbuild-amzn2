4678701be6ed0eaf19ecdd8ae39597e3c1e23dbf
bootswithdefer <github@dotd.com>
2023-03-24 08:15:26 -0700

    update build script

--- build.sh
+++ build.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
+#docker buildx build --platform linux/amd64 -t bootswithdefer/rpmbuild-amzn2:latest .
+#docker buildx build --platform linux/arm64 -t bootswithdefer/rpmbuild-amzn2:latest .
 docker buildx build --platform linux/amd64,linux/arm64 -t bootswithdefer/rpmbuild-amzn2:latest --push .
